3a10ba5ecf97ac0379f8021bb8645b528f47a268
Flint <flint@iMac1.local>
2019-04-08 12:01:12 -0400

    Finished AboutMe lab from book

---
+++ Documents/ios_unit_three/AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Flint on 4/8/19.
+//  Copyright © 2019 Flint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
