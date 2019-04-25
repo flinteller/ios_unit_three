daefcfe76f2e3f11d567b87e68ff38cce0371413
Flint <flint@iMac1.local>
2019-04-25 11:30:39 -0400

    Finished personality quiz

---
+++ Documents/ios_unit_three/Personality Quiz/Personality Quiz/IntroductionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Flint on 4/15/19.
+//  Copyright © 2019 Flint. All rights reserved.
+//  This file handles all the commands that have to do with the into screen
+
+import UIKit
+
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
---
+++ Documents/ios_unit_three/Personality Quiz/Personality Quiz/ResultsViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ResultsViewController.swift
+//  Personality Quiz
+//
+//  Created by Flint on 4/17/19.
+//  Copyright © 2019 Flint. All rights reserved.
+//  This file handles the last screen, which displays the results 
+
+import UIKit
+
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    
+    var responses: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers: [AnimalType: Int] = [:]
+        let responseTypes = responses.map{$0.type}
+        for response in responseTypes {
+            frequencyOfAnswers[response] = (frequencyOfAnswers[response] ?? 0) + 1
+        }
+            let frequentAnswersSorted = frequencyOfAnswers.sorted(by: {(pair1, pair2) -> Bool in
+                return pair1.value > pair2.value
+        })
+            let mostCommonAnswer = frequencyOfAnswers.sorted{$0.1 > $1.1 }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+        
+        }
+        
+    
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
